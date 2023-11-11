b93395f75a53f07a49366dbafff49307246b09ab
aadk979 <138833125+aadk979@users.noreply.github.com>
2023-11-11 17:44:45 +0800

    Update Testserver.js

--- Testserver.js
+++ Testserver.js
@@ -4,6 +4,43 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 let username;
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12661021',
+    password: 'sR9jZsnCs1',
+    database: 'sql12661021',
+});
+
+// Open the connection
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+// Insert a name into the table
+const nameToInsert = 'John Doe';
+connection.query('INSERT INTO your_table_name (name) VALUES (?)', [nameToInsert], (err, results) => {
+    if (err) {
+        console.error('Error inserting name:', err);
+        return;
+    }
+    console.log('Name inserted successfully. Inserted ID:', results.insertId);
+});
+
+// Close the connection after inserting
+connection.end((err) => {
+    if (err) {
+        console.error('Error closing connection:', err);
+        return;
+    }
+    console.log('Connection closed');
+});
 
 const server = http.createServer(app);
 
