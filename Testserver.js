3fdf77bc40003a96db781b5a22f1199dafac8eae
aadk979 <138833125+aadk979@users.noreply.github.com>
2023-11-12 19:25:00 +0800

    Update Testserver.js

--- Testserver.js
+++ Testserver.js
@@ -4,7 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 let username;
-const systemstorage = {};
+let systemstorage = {};
 const server = http.createServer(app);
 
 const io = new Server(server, {
