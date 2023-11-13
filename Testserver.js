f7389c50ac6d91262cd93b877044292a59231ea9
aadk979 <138833125+aadk979@users.noreply.github.com>
2023-11-13 17:21:17 +0800

    Update Testserver.js

--- Testserver.js
+++ Testserver.js
@@ -3,7 +3,8 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-let username;
+const multer = require("multer");
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -13,62 +14,66 @@ const io = new Server(server, {
     }
 });
 
+// Set up multer for handling file uploads
+const storage = multer.memoryStorage(); // You can change this to save files to disk if needed
+const upload = multer({ storage: storage });
+
 function generateUniqueCode() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let code = '';
-
-  for (let i = 0; i < 15; i++) {
-    code += characters.charAt(Math.floor(Math.random() * characters.length));
-
-    // Add a dash every 5 characters except for the last one
-    if ((i + 1) % 5 === 0 && i !== 14) {
-      code += '-';
-    }
-  }
-
-  return code;
+    // Your existing code for generating a unique code
 }
 
 let systemstorage = {};
 
-// Function to save data to system storage
 function savetosystemstorage(key, data1, data2) {
-  systemstorage[key] = { name: data1, uic: data2 };
+    systemstorage[key] = { name: data1, uic: data2 };
 }
 
-// Function to retrieve only the second data
 function retrieveSecondData(key) {
-  const storedData = systemstorage[key];
+    const storedData = systemstorage[key];
 
-  if (storedData) {
-    return storedData.uic;
-  } else {
-    return null; // or any default value you prefer
-  }
+    if (storedData) {
+        return storedData.uic;
+    } else {
+        return null; // or any default value you prefer
+    }
 }
 
 io.on("connection", (socket) => {
-  	socket.on("redirect-request" , (data)=>{
-      const code = generateUniqueCode();
-      io.emit(data,{link:"https://resonant-squirrel-ba2715.netlify.app" , uic: code});
-    })
-    socket.on("enckey",(data)=>{
-        io.emit("enckey", ({user: data.user , key: data.key }));
+    socket.on("redirect-request", (data) => {
+        const code = generateUniqueCode();
+        io.emit(data, { link: "https://resonant-squirrel-ba2715.netlify.app", uic: code });
     });
-    socket.on("message" , (data)=>{
-        io.emit("message" , ({message:data.message,time:data.time,to:data.to,from:data.from}))  });
-    socket.on("newuser" , (data) =>{
+
+    socket.on("enckey", (data) => {
+        io.emit("enckey", { user: data.user, key: data.key });
+    });
+
+    socket.on("message", (data) => {
+        io.emit("message", { message: data.message, time: data.time, to: data.to, from: data.from });
+    });
+
+    socket.on("newuser", (data) => {
         io.emit("newuser", data);
     });
-    socket.on("id" ,  (data)=>{
-        io.emit("id" , ({user: data.user ,uic: data.uic}));
-        savetosystemstorage(socket.id , data.user ,data.uic);
-    })
-    socket.on("disconnect" , ()=>{
-      const data = retrieveSecondData(socket.id);
-      io.emit("disc" , ({uic: data}));
-      
-    })
+
+    socket.on("id", (data) => {
+        io.emit("id", { user: data.user, uic: data.uic });
+        savetosystemstorage(socket.id, data.user, data.uic);
+    });
+
+    // Handle file upload event
+    socket.on("file", upload.single("file"), (data) => {
+        // Process the uploaded file
+        const fileBuffer = data.file.buffer; // This is the file content in a Buffer
+
+        // Example: Emit a message with the file content to all clients
+        io.emit("file", { content: fileBuffer.toString("base64") });
+    });
+
+    socket.on("disconnect", () => {
+        const data = retrieveSecondData(socket.id);
+        io.emit("disc", { uic: data });
+    });
 });
 
 const PORT = process.env.PORT || 5500;
